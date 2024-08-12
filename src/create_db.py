6e01295d9eafa133892f05bdb61b25d050aba497
Павел Сергеевич <p.bitiakov.official@gmail.com>
2024-08-12 16:58:53 +0300

    Модификация кода

--- src/create_db.py
+++ src/create_db.py
@@ -19,9 +19,10 @@ def create_data_base(database_name, params) -> None:
 
     with psycopg2.connect(dbname=database_name, **params) as conn:
         with conn.cursor() as cur:
+            # Создание таблицы companies с первичным ключом
             cur.execute("""
             CREATE TABLE IF NOT EXISTS companies (
-            company_id int,
+            company_id SERIAL PRIMARY KEY,
             company_name VARCHAR(255),
             company_url TEXT
             )
@@ -30,14 +31,17 @@ def create_data_base(database_name, params) -> None:
         with conn.cursor() as cur:
             cur.execute("""
             CREATE TABLE IF NOT EXISTS vacancies (
-            company_name VARCHAR(255),
-            job_title VARCHAR,
-            link_to_vacancy TEXT,
-            salary_from int,
-            currency VARCHAR(20),
-            experience TEXT,
-            description TEXT, 
-            requirement TEXT
+                vacancy_id SERIAL PRIMARY KEY,
+                company_id INT,
+                company_name VARCHAR(255),
+                job_title VARCHAR,
+                link_to_vacancy TEXT,
+                salary_from INT,
+                currency VARCHAR(20),
+                experience TEXT,
+                description TEXT,
+                requirement TEXT,
+                FOREIGN KEY (company_id) REFERENCES companies(company_id) ON DELETE CASCADE
             )
             """)
     conn.commit()
@@ -48,20 +52,37 @@ def save_data_to_db(data, database_name, params) -> None:
     """
     Заполнение таблиц companies и vacancies данными
     """
-    insert_q = """
-        INSERT INTO vacancies (company_name, job_title, link_to_vacancy, salary_from, currency, experience, description,
-        requirement)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-        """
-    insert_q_2 = """
-                 INSERT INTO companies (company_id, company_name, company_url) VALUES (%s, %s, %s)
-                 """
+    insert_company_q = """
+           INSERT INTO companies (company_name, company_url) VALUES (%s, %s) RETURNING company_id
+       """
+    insert_vacancy_q = """
+           INSERT INTO vacancies (company_id, company_name, job_title, link_to_vacancy, salary_from, currency, experience, description,
+                                  requirement)
+           VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+       """
+    select_company_q = """
+           SELECT company_id FROM companies WHERE company_name = %s AND company_url = %s
+       """
+
     with psycopg2.connect(dbname=database_name, **params) as conn:
         with conn.cursor() as cur:
             for vacancy in data:
-                cur.execute(insert_q_2, (vacancy['company_id'], vacancy['company_name'], vacancy['company_url']))
-                cur.execute(insert_q, (vacancy['company_name'], vacancy['job_title'], vacancy['link_to_vacancy'],
-                                       vacancy['salary_from'], vacancy['currency'], vacancy["experience"],
-                                       vacancy['description'], vacancy['requirement']))
+                # Проверяем, существует ли компания в базе данных
+                cur.execute(select_company_q, (vacancy['company_name'], vacancy['company_url']))
+                company_id = cur.fetchone()
+
+                # Если компания уже существует, берем ее ID
+                if company_id:
+                    company_id = company_id[0]
+                # Если компании нет, вставляем ее и получаем ID
+                else:
+                    cur.execute(insert_company_q, (vacancy['company_name'], vacancy['company_url']))
+                    company_id = cur.fetchone()[0]
+
+                # Теперь вставляем вакансию с company_id и company_name
+                cur.execute(insert_vacancy_q, (company_id, vacancy['company_name'], vacancy['job_title'], vacancy['link_to_vacancy'],
+                                               vacancy['salary_from'], vacancy['currency'],
+                                               vacancy["experience"], vacancy['description'],
+                                               vacancy['requirement']))
     conn.commit()
     conn.close()
--- src/db_manager.py
+++ src/db_manager.py
@@ -20,13 +20,6 @@ class DBManager:
         self.cur.close()
         self.conn.close()
 
-
-class VacancyService:
-    """Класс для работы с данными о вакансиях"""
-
-    def __init__(self, db_manager: DBManager):
-        self.db_manager = db_manager
-
     def get_companies_and_vacancies_count(self) -> dict:
         """Получает список всех компаний и количество вакансий у каждой компании"""
         query = """
@@ -34,7 +27,7 @@ class VacancyService:
         FROM vacancies
         GROUP BY company_name
         """
-        rows = self.db_manager.execute_query(query)
+        rows = self.execute_query(query)
         return {row[0]: row[1] for row in rows}
 
     def get_all_vacancies(self) -> list:
@@ -46,12 +39,12 @@ class VacancyService:
         SELECT company_name, job_title, salary_from, currency, link_to_vacancy 
         FROM vacancies
         """
-        return self.db_manager.execute_query(query)
+        return self.execute_query(query)
 
     def get_avg_salary(self):
         """Возвращает среднюю зарплату по вакансиям из БД"""
         query = "SELECT AVG(salary_from) FROM vacancies"
-        rows = self.db_manager.execute_query(query)
+        rows = self.execute_query(query)
         return rows if rows else None
 
     def get_vacancies_with_higher_salary(self) -> list:
@@ -61,7 +54,7 @@ class VacancyService:
         FROM vacancies
         WHERE salary_from > (SELECT AVG(salary_from) FROM vacancies)
         """
-        return self.db_manager.execute_query(query)
+        return self.execute_query(query)
 
     def get_vacancies_with_keyword(self, keyword) -> list:
         """
@@ -69,4 +62,4 @@ class VacancyService:
         переданные в метод слова, например 'python'
         """
         query = """SELECT * FROM vacancies WHERE LOWER(job_title) LIKE %s"""
-        return self.db_manager.execute_query(query, ('%' + keyword.lower() + '%',))
+        return self.execute_query(query, ('%' + keyword.lower() + '%',))
